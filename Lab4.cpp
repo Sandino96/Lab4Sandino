d8052f32acc490534cbff8355e2cdd3db2df509d
Rodrigo José Sandino Salinas <sandinorodrigo@unitec.edu>
2016-05-13 14:40:21 -0600

    Avance primer ejercicio

--- Lab4.cpp
+++ Lab4.cpp
@@ -1,14 +1,18 @@
 #include <iostream>
 #include <cstring>
+#include <cmath>
 
 using std::cout;
 using std::cin;
 using std::endl;
 
 void menu();
+int* ejercicio1(int*,int);
+void ejercicio2();
 
 int main(int argc, char*agrv[]){
-	
+	menu();
+	return 0;
 }
 
 void menu(){
@@ -17,4 +21,38 @@ void menu(){
 	cout <<  "2.-Ejercicio 2" << endl;
 	cout <<  "Opcion: ";
 	cin >> opc;
+	switch(opc){
+		case 1:
+			int* polinomio = new int[3];
+			int size = 3;
+			cout << "Polinomio";
+			cout << "X^2 = ";
+			cin >> polinomio[0];
+			cout << "X = ";
+			cin >> polinomio[1];
+			cout << "Constante = ";
+			cin >> polinomio[2];
+			for (int i = 0; i < 2; i++){
+				ejercicio1(polinomio,size)[i] *= -1;
+			}
+			cout << "X^2 " << polinomio[0] << " + X " << polinomio[1] << " + " << polinomio[2] << endl;
+			cout << "(X + " << ejercicio1(polinomio,size)[0] << ")(X + " << ejercicio1(polinomio,size)[1] << ")" << endl;
+			delete [] polinomio;
+	}
+}
+
+int* ejercicio1(int* polinomio, int size){
+	if(polinomio[0] == 0){
+		return polinomio;
+	} else {
+		int* resultado = new int[2];
+		int raizCuadrada = (pow(polinomio[1],2) + 4 * polinomio[0] * polinomio[2]);
+		if(raizCuadrada < 0){
+			return polinomio;
+		} else {
+			resultado[0] = (-1 * polinomio[1] + sqrt(raizCuadrada)/ 2 * polinomio[0]);
+			resultado[1] = (-1 * polinomio[1] - sqrt(raizCuadrada)/ 2 * polinomio[0]);
+			return resultado;
+		}
+	}
 }
