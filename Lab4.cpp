3549f3ba20f92225c9b3095829e1bfeadab482cc
Rodrigo José Sandino Salinas <sandinorodrigo@unitec.edu>
2016-05-13 13:47:09 -0600

    Sirve

---
+++ Lab4.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <cstring>
+
+using std::cout;
+using std::cin;
+using std::endl;
+
+void menu();
+
+int main(int argc, char*agrv[]){
+	
+}
+
+void menu(){
+	int opc;
+	cout <<  "1.-Ejercicio 1" << endl;
+	cout <<  "2.-Ejercicio 2" << endl;
+	cout <<  "Opcion: ";
+	cin >> opc;
+}
